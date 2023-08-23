50057d06d99253d480feca7323683b9509ca1a2e
MAUIoxo <140683727+MAUIoxo@users.noreply.github.com>
2023-08-23 12:11:24 +0200

    Add files via upload

---
+++ LinearGradient_16392/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if (height > 0)
+            {
+                BackgroundImage.HeightRequest = height * 0.7;
+            }
+        }
+    }
+}
